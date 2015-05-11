3c49b085d45754d1d6ccd86d08f0f653db6cc43f
Dave Schumaker <daveschumaker@users.noreply.github.com>
2015-05-11 14:33:31 -0700

    Solution for _.last

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,11 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    console.log(array);
+    console.log("SLICE: " + n );
+    console.log(n === undefined ? array[array.length - 1] : array.slice(n > array.length ? 0 : array.length - n, n+1));
+    console.log("\n\n");
+    return n === undefined ? array[array.length - 1] : array.slice(n > array.length - 1 ? 0 : array.length - n, n+1);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
